58505922dafe9343f5e04e9c526a242f39349640
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2012-02-29 03:51:00 +0100

    Allowed setting config defaults through the application config.

--- fuelphp/app/application.php
+++ fuelphp/app/application.php
@@ -8,7 +8,13 @@ class App extends Application\Base
 	public function router()
 	{
 		$this->add_route('/', 'Welcome');
-
 		$this->add_route('GET /(.*)', 'Welcome/catchall/$1');
+	}
+
+	public function config()
+	{
+		return array(
+			'log_level' => 4,
+		);
 	}
 }
