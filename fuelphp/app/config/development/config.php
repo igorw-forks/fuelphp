9d7d1c17ef56c77b83e811d34adb0977657638c6
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2012-03-02 23:55:09 +0100

    Added some config and gitignored app logs, cache & tmp dirs.

--- fuelphp/app/config/development/config.php
+++ fuelphp/app/config/development/config.php
@@ -1,5 +1,19 @@
 <?php
 
+use Fuel\Kernel\Log;
+
 return array(
-	'log_level' => 4,
+	'log' => array(
+		'level' => 4,
+		'date_format' => 'Y-m-d H:i:s',
+		'threshold' => Log::L_ALL & ~Log::L_INFO,
+		// 'path' => $app->loader->path().'resources/logs/',
+	),
+
+	'file' => array(
+		'chmod' => array(
+			'files' => 0666,
+			'folders' => 0777,
+		),
+	),
 );
