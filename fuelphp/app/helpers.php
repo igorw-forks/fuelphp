01475ce25370068eabc6abf867bcaffe2c2643f3
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2012-03-06 17:36:28 +0100

    _app() and _env() helper functions are unnecessary (aside from breaking encapsulation), if you want global scope there already are $app and $env variables declared there.
    Still to be decided whether the remaining _forge(), _req() and _obj() should even remain.

--- fuelphp/app/helpers.php
+++ fuelphp/app/helpers.php
@@ -6,39 +6,6 @@
  */
 
 /**
- * Fetch the Fuel Environment
- *
- * @param   null|string  $var
- * @return  Fuel\Kernel\Environment
- *
- * @since  2.0.0
- */
-function _env($var = null)
-{
-	$env = Fuel\Kernel\Environment::instance();
-	return is_null($var) ? $env : $env->{$var};
-}
-
-/**
- * Return the current active Application
- *
- * @param   null|string  $var
- * @return  Fuel\Kernel\Application\Base
- *
- * @since  2.0.0
- */
-function _app($var = null)
-{
-	if ( ! ($app = _env()->active_application()))
-	{
-		return null;
-	}
-
-	return $var ? $app->{$var} : $app;
-}
-
-
-/**
  * Return the current active Request
  *
  * @param   null|string  $var
@@ -48,7 +15,10 @@ function _app($var = null)
  */
 function _req($var = null)
 {
-	$req = ($app = _app()) ? $app->active_request() : null;
+	global $env, $app;
+	$app = $env->active_application() ?: $app;
+
+	$req = $app ? $app->active_request() : null;
 	if ( ! $req)
 	{
 		return null;
@@ -67,7 +37,10 @@ function _req($var = null)
  */
 function _forge($classname)
 {
-	return call_user_func_array(array(_app() ?: _env(), 'forge'), func_get_args());
+	global $env, $app;
+	$app = $env->active_application() ?: $app;
+
+	return call_user_func_array(array($app ?: $env, 'forge'), func_get_args());
 }
 
 /**
@@ -79,6 +52,9 @@ function _forge($classname)
  */
 function _obj($classname, $name = null)
 {
-	$dic = _app('dic') ?: _env('dic');
+	global $env, $app;
+	$app = $env->active_application() ?: $app;
+
+	$dic = $app ? $app->dic : $env->dic;
 	return $dic->get_object($classname, $name);
 }
